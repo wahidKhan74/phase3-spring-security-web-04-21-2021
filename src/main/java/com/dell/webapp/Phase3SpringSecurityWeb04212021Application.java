b3678f62e7f74f572bfb72717253e36a6be981f7
Waheed Khan <wahid.khan74@gmail.com>
2021-04-21 05:56:28 +0000

    spring security + thymeleaf

---
+++ src/main/java/com/dell/webapp/Phase3SpringSecurityWeb04212021Application.java
@@ -0,0 +1,13 @@
+package com.dell.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3SpringSecurityWeb04212021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3SpringSecurityWeb04212021Application.class, args);
+	}
+
+}
